4ed5cb094555f5f3e5b7efb38581378b433ccaff
Peter Kester <peter.kester@marklogic.com>
2021-06-17 15:03:52 +0200

    echos verwijderd en dist gebouwd

--- buildrun.sh
+++ buildrun.sh
@@ -7,7 +7,7 @@ then
   NETWORK="--network=$1"
 fi
 
-echo docker build -t ml-lodlive .
-echo docker stop ml-lodlive
-echo docker rm ml-lodlive
-echo docker run --name ml-lodlive $NETWORK -p 8888:80 ml-lodlive
+docker build -t ml-lodlive .
+docker stop ml-lodlive
+docker rm ml-lodlive
+docker run --name ml-lodlive $NETWORK -p 8888:80 ml-lodlive
